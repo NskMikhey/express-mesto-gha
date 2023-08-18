1a5148e5cc5624262d467a442fcf44a48dfae4c7
nskmikhey <NskMikhei@yandex.ru>
2023-08-18 21:13:26 +0700

    fix login

--- controllers/users.js
+++ controllers/users.js
@@ -134,15 +134,12 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
-
-      // вернём токен
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
-      });
-      res.send({ token: `Bearer ${token}` });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+        { expiresIn: '7d' },
+      );
+      res.send({ token });
     })
     .catch(() => {
       // ошибка аутентификации
