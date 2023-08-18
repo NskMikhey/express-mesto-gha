d903edc990be04659439cc4884643c64fb6ecb30
nskmikhey <NskMikhei@yandex.ru>
2023-08-18 19:28:25 +0700

    refactor token

--- controllers/users.js
+++ controllers/users.js
@@ -134,14 +134,11 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
-
-      // вернём токен
-      res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
-      });
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+        { expiresIn: '7d' },
+      );
       res.send({ token: `Bearer ${token}` });
     })
     .catch(() => {
